01ae42e884686d9a26b8df6922396489148682fe
Biguá <bigua.kun@gmail.com>
2015-02-10 21:05:11 -0200

    Menu lateral, Action Bar

--- MainActivity.java
+++ MainActivity.java
@@ -1,23 +1,76 @@
 package me.bigua.comiccollector;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import br.liveo.interfaces.NavigationLiveoListener;
 import br.liveo.navigationliveo.NavigationLiveo;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class MainActivity extends NavigationLiveo {
+public class MainActivity extends NavigationLiveo implements NavigationLiveoListener {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onUserInformation() {
+        //User information here
+//        this.mUserName.setText("Rudson Lima");
+//        this.mUserEmail.setText("rudsonlive@gmail.com");
+//        this.mUserPhoto.setImageResource(R.drawable.ic_rudsonlive);
+//        this.mUserBackground.setImageResource(R.drawable.ic_user_background);
 
-        //base
 
-        BaseHelper dbhelper = new BaseHelper(this);
-        dbhelper.getWritableDatabase();
+        View mCustomHeader = getLayoutInflater().inflate(R.layout.custom_header_user, this.getListView(), false);
+        ImageView imageView = (ImageView) mCustomHeader.findViewById(R.id.imageView);
+        this.addCustomHeader(mCustomHeader); //This will add the new header and remove the default user header
+    }
+
+
+    @Override
+    public void onInt(Bundle savedInstanceState) {
+        //Creation of the list items is here
+
+        // set listener {required}
+        this.setNavigationListener(this);
+
+        // name of the list items
+
+        List<String> mListNameItem = new ArrayList<>();
+        mListNameItem.add(0, "lalala");
+        mListNameItem.add(1, "lalala");
+        mListNameItem.add(2, "lalala");
+        mListNameItem.add(3, "lalala");
+        mListNameItem.add(4, "lalala"); //This item will be a subHeader
+        mListNameItem.add(5, "lalala");
+        mListNameItem.add(6, "lalala");
+
+        // icons list items
+        List<Integer> mListIconItem = new ArrayList<>();
+        mListIconItem.add(0, 0);
+        mListIconItem.add(1, 0); //Item no icon set 0
+        mListIconItem.add(2, 0); //Item no icon set 0
+        mListIconItem.add(3, 0);
+        mListIconItem.add(4, 0); //When the item is a subHeader the value of the icon 0
+        mListIconItem.add(5, 0);
+        mListIconItem.add(6, 0);
+
+        //{optional} - Among the names there is some subheader, you must indicate it here
+        List<Integer> mListHeaderItem = new ArrayList<>();
+        mListHeaderItem.add(4);
+
+        //{optional} - Among the names there is any item counter, you must indicate it (position) and the value here
+
+//        SparseIntArray mSparseCounterItem = new SparseIntArray(); //indicate all items that have a counter
+//        mSparseCounterItem.put(0, 7);
+//        mSparseCounterItem.put(6, 250);
+
+        //If not please use the FooterDrawer use the setFooterVisible(boolean visible) method with value false
+//        this.setFooterInformationDrawer(R.string.settings, R.drawable.ic_settings_black_24dp);
+        this.setFooterNavigationVisible(false);
+        this.setNavigationAdapter(mListNameItem, mListIconItem, mListHeaderItem, null);
     }
 
 
@@ -42,4 +95,47 @@ public class MainActivity extends NavigationLiveo {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    @Override
+    public void onClickUserPhotoNavigation(View view) {
+
+    }
+
+    @Override
+    public void onClickFooterItemNavigation(View view) {
+
+    }
+
+    @Override
+    public void onPrepareOptionsMenuNavigation(Menu menu, int i, boolean b) {
+
+    }
+
+    @Override
+    public void onItemClickNavigation(int position, int container) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        switch (position) {
+            case 0:
+                fragmentTransaction.replace(R.id.container, MainFragment.newInstance(position + 1)).commit();
+                break;
+            case 1:
+//                fragmentTransaction.replace(R.id.container, ProfileFragment.newInstance(position + 1)).commit();
+                break;
+            case 2:
+//                fragmentTransaction.replace(R.id.container, PrescriptionFragment.newInstance(position + 1)).commit();
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+//                Intent intent = new Intent(this, LoginActivity.class);
+//                startActivity(intent);
+                finish();
+                break;
+        }
+
+    }
 }
+
---
+++ MainFragment.java
@@ -0,0 +1,37 @@
+package me.bigua.comiccollector;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Bigua on 2/10/15.
+ */
+public class MainFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+
+    public static MainFragment newInstance(int sectionNumber) {
+        MainFragment fragment = new MainFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(
+                R.layout.fragment_main,
+                container, false);
+
+
+        return rootView;
+    }
+
+
+}
